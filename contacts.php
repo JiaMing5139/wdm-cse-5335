a046096d1a8a43d8f81dbbfe8c83ad31af66e642
Ashutosh Mehta <ashutoshmehta65@gmail.com>
2020-04-14 12:11:17 -0500

    [+]Update contacts.php

--- contacts.php
+++ contacts.php
@@ -161,6 +161,29 @@ if(isset ($_POST['submit']))
         }
 
     }
+        // require_once('phpmailer/PHPMailerAutoload.php');
+        // try
+        // {
+        //     $mail=new PHPMailer(true);
+        //     $mail->IsSMTP(); 
+        //     $mail->SMTPDebug = 2;
+        //     $mail->SMTPAuth=true;
+        //     $mail->SMTPSecure='ssl';
+        //     $mail->Host='smtp.gmail.com';
+        //     $mail->SMTPAuth = true;
+        //     $mail->Port=465;
+        //     $mail->isHTML();
+        //     $mail->Username='centro.mijares@gmail.com';
+        //     $mail->Password='abcd1234@';
+        //     $mail->Subject=$subject;
+        //     $mail->SetFrom("ashutoshmehta65@gmail.com", $name);
+        //     $mail->Body=$msg;
+        //     $mail->AddAddress('centro.mijares@gmail.com');
+        //     $mail->send();
+        // }
+        // catch(Exception $e){
+        //     echo $e->getMessage();
+        // }    
 }
 ?>
 
